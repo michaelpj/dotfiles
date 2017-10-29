0bb788a49889dd3c3f0de62f6bfd4cafc62b64fa
Michael Peyton Jones <me@michaelpj.com>
2017-10-29 11:02:44 +0000

    Not using guake anymore

--- guake/set_colours.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-gconftool-2 -s -t string /apps/guake/style/background/color '#000000000000'
-gconftool-2 -s -t string /apps/guake/style/font/color '#ffffffffffff'
-gconftool-2 -s -t string /apps/guake/style/font/palette '#000000000000:#e5e522222222:#a6a6e3e32d2d:#fcfc95951e1e:#c4c48d8dffff:#fafa25257373:#6767d9d9f0f0:#f2f2f2f2f2f2:#555557575353:#efef29292929:#8cfbece22f41:#fcfce9e94f4f:#ad177b7fe24a:#cd2516195921:#3434e2e2e2e2:#e84be84be7c2'
-gconftool-2 -s -t string /apps/guake/style/font/style 'DejaVu Sans Mono 11'
